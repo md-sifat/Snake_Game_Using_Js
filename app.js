a27c645efcaf12d816cfe1fdac6e688467823aa5
mdsifat <mdsifat7252@gmail.com>
2023-08-15 12:30:52 +0600

    game over functionality debugged

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const board = document.querySelector(".game_board");
+const scoreBoard = document.querySelector(".cscore");
+const hscore = document.querySelector(".hscore");
 
 let foodY = 10 , foodX=13;
 let snakeY=10 , snakeX=10;
 let vX=0 , vY=0;
 let gameOver=false;
-let score = 0;
+let score = 0 , highscore=25;
 let setIntervalId;
 let snakeBody = [];
 
 const gameOver_Ops = () =>{
     clearInterval(setIntervalId);
     alert(`Game Over. Score ${score} . Press Ok to Play again`);
+
     location.reload();
 }
 
@@ -42,11 +45,14 @@ const initGame = () =>{
     let markUp = `<div class="food" style = "grid-area : ${foodY} / ${foodX}"></div>`;
     snakeX+=vX;
     snakeY+=vY;
-    if(snakeX>40 || snakeY>40 || snakeX<0 && snakeY<0){
+    if(snakeX>40 || snakeY>40 || snakeX<0 || snakeY<0){
         gameOver=true;
     }
-    if(snakeX === foodX && snakeY==foodY){
+    if(snakeX === foodX && snakeY===foodY){
         score++;
+        highscore = Math.max(score , highscore);
+        scoreBoard.innerHTML = `Score : ${score}`;
+        hscore.innerHTML = `High Score : ${highscore}`;
         changeFood();
         snakeBody.push([snakeX , snakeY]);
         console.log(snakeBody);
