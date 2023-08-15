b4162e4e76b21a98d6b8dbd1737eedbc3b00a155
mdsifat <mdsifat7252@gmail.com>
2023-08-15 11:10:26 +0600

    snake added

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
 const board = document.querySelector(".game_board");
 
-let foody = 12 , foodx=10;
+let foodY = 10 , foodX=13;
+let snakeY=10 , snakeX=10;
+let vX=0 , vY=0;
+let snakeBody = [];
+
+
+
+const changeFood = () =>{
+    foodX = Math.floor(Math.random() * 40 +1);
+    foodY = Math.floor(Math.random() * 40 +1);
+}
+const moveSnake = e =>{
+    console.log(e);
+    if(e.key === "ArrowUp"){
+        vX = 0;
+        vY = -1;
+    }else if(e.key === "ArrowDown"){
+        vX = 0;
+        vY = 1;
+    }else if(e.key === "ArrowLeft"){
+        vX = -1;
+        vY = 0;
+    }else if(e.key === "ArrowRight"){
+        vX = 1;
+        vY = 0;
+    }
+    initGame();
+}
 const initGame = () =>{
-    const markUp = `<div class="food" style = "grid-area : ${foody} / ${foodx}"></div>`;
-    const food = document.createElement("div");
-    food.innerHTML = markUp;
-    board.appendChild(food);
+    let markUp = `<div class="food" style = "grid-area : ${foodY} / ${foodX}"></div>`;
+    snakeX+=vX;
+    snakeY+=vY;
+    if(snakeX === foodX && snakeY==foodY){
+        changeFood();
+        snakeBody.push([snakeX , snakeY]);
+        console.log(snakeBody);
+    }
+    for(let i=snakeBody.length-1 ; i>=0 ; i--){
+        snakeBody[i]=snakeBody[i-1];
+    }
+
+    snakeBody[0] = [snakeX , snakeY]; 
+    for(let i=0 ; i<snakeBody.length ; i++){
+        markUp += `<div class="snake" style = "grid-area : ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+
+    }
+
+    board.innerHTML = markUp;
     
 }
+
+
+changeFood();
+setInterval(initGame, 125); 
+document.addEventListener('keydown' , moveSnake);
